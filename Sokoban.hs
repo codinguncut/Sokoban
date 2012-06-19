de4c9a1ee6d3be715933809867b26b850ff330f6
jonas <jonas@jonas-VirtualBox.(none)>
2012-06-20 10:04:50 +1030

    general clean-up

--- Sokoban.hs
+++ Sokoban.hs
@@ -1,7 +1,5 @@
 module Sokoban where
 
-import Test.HUnit
-import Test.QuickCheck
 import Prelude hiding (Either(..))
 import Data.List (sort, delete)
 import Control.Monad (forM_)
@@ -21,7 +19,7 @@ data World = World  {wWalls
                     ,wWorker    :: Coord
                     ,wMax       :: Coord
                     ,wSteps     :: Int
-                    } deriving (Show)
+                    }
 
 emptyWorld = World {wWalls    = []
                    ,wCrates   = []
@@ -31,29 +29,26 @@ emptyWorld = World {wWalls    = []
                    ,wSteps    = 0
                    }
 
-
 add :: Coord -> Input -> Coord
 add (x,y) input = 
   case input of
-    Up    -> (x   , y-1)
-    Down  -> (x   , y+1)
-    Left  -> (x-1 , y)
-    Right -> (x+1 , y)
+    Up    -> (x  , y-1)
+    Down  -> (x  , y+1)
+    Left  -> (x-1, y  )
+    Right -> (x+1, y  )
+
+isWall :: World -> Coord -> Bool
+isWall world coord = elem coord (wWalls world)
+
+isCrate :: World -> Coord -> Bool
+isCrate world coord = elem coord (wCrates world)
+
+isStorage :: World -> Coord -> Bool
+isStorage world coord = elem coord (wStorages world)
 
 
-level = unlines
-  ["    #####"
-  ,"    #   #"
-  ,"    #o  #"
-  ,"  ###  o##"
-  ,"  #  o o #"
-  ,"### # ## #   ######"
-  ,"#   # ## #####  ..#"
-  ,"# o  o          ..#"
-  ,"##### ### #@##  ..#"
-  ,"    #     #########"
-  ,"    #######"
-  ]
+---
+
 
 loadLevel :: String -> World
 loadLevel str = foldl consume (emptyWorld{wMax = maxi}) elems
@@ -73,45 +68,46 @@ loadLevel str = foldl consume (emptyWorld{wMax = maxi}) elems
             otherwise -> error (show elt ++ " not recognized")
   
 
-displayWorld :: World -> IO ()
-displayWorld w = putStrLn . unlines . map (map func) $ coords
-  where (maxX, maxY)  = wMax w
-        coords        = [[(x,y) | x <- [0..maxX]] | y <- [0..maxY]]
-        isWorker w c  = wWorker w == c
-        func c        =
-          case () of () | isCrate w c && isStorage w c   -> '*'
-                        | isWorker w c && isStorage w c  -> '+'
-                        | isWall w c     -> '#'
-                        | isWorker w c   -> '@'
-                        | isCrate w c    -> 'o'
-                        | isStorage w c  -> '.'
-                        | otherwise      -> ' '
+instance Show World where
+  show w = unlines chars
+    where (maxX, maxY)  = wMax w
+          chars         = [[func (x,y) | x <- [0..maxX]] | y <- [0..maxY]]
+          isWorker w c  = wWorker w == c
+          func c 
+            | isCrate   w c && isStorage w c  = '*'
+            | isWorker  w c && isStorage w c  = '+'
+            | isWall    w c                   = '#'
+            | isWorker  w c                   = '@'
+            | isCrate   w c                   = 'o'
+            | isStorage w c                   = '.'
+            | otherwise                       = ' '
 
 
--- modifyWorld is only called with a valid input move
 modifyWorld :: World -> Input -> World
-modifyWorld world input =
-  case () of () | isCrate world newPos  -> moveCrate world' newPos newPos'
-                | otherwise             -> world'
-  where oldPos  = wWorker world
+modifyWorld world input 
+  | isWall    world newPos  = world
+  | isCrate   world newPos  = 
+      if isCrate world newPos' || isWall world newPos'
+        then world
+        else moveCrate world' newPos newPos'
+  | otherwise               = world'
+  where moveCrate w old new = w{wCrates = new:delete old (wCrates w)}
+        
+        oldPos  = wWorker world
         newPos  = add oldPos input
         newPos' = add newPos input
         world'  = world{wWorker = newPos, wSteps = wSteps world + 1}
-        moveCrate w old new = w{wCrates = new:delete old (wCrates world)}
+        
+
+isFinished :: World -> Bool
+isFinished world = 
+  sort (wCrates world) == sort (wStorages world)
 
 
-isValid :: World -> Input -> Bool
-isValid world input =
-  case () of 
-    ()  | isWall    world newPos -> False
-        | isCrate   world newPos -> 
-            not (isCrate world newPos') && not (isWall world newPos')
-        | otherwise        -> True
-  where oldPos  = wWorker world
-        newPos  = add oldPos input
-        newPos' = add newPos input
+---
 
 
+-- get characters until valid character is received
 getInput :: IO Input
 getInput = do
   char <- getChar
@@ -123,22 +119,14 @@ getInput = do
     otherwise -> getInput
 
 
-isWall :: World -> Coord -> Bool
-isWall world coord = elem coord (wWalls world)
-
-
-isCrate :: World -> Coord -> Bool
-isCrate world coord = elem coord (wCrates world)
-
-
-isStorage :: World -> Coord -> Bool
-isStorage world coord = elem coord (wStorages world)
-
-
-isFinished :: World -> Bool
-isFinished world = 
-  sort (wCrates world) == sort (wStorages world)
-
+gameLoop world = do
+  print world
+  input <- getInput
+  let world' = modifyWorld world input
+  if isFinished world'
+    then print world' >> print "well done!"
+    else gameLoop world'
+  
 
 main :: IO ()
 main = do
@@ -149,27 +137,19 @@ main = do
   gameLoop $ loadLevel level
 
 
-gameLoop world = do
-  displayWorld world
-  input <- getInput
-  let world' = if isValid world input
-                 then modifyWorld world input
-                 else world
-  if isFinished world'
-    then displayWorld world' >> print "well done!"
-    else gameLoop world'
-  
+---
 
 
--- TESTS
-
-testsSoko = TestList $ map TestCase
-  [assertEqual ""  1 
-                   1 
+level = unlines
+  ["    #####"
+  ,"    #   #"
+  ,"    #o  #"
+  ,"  ###  o##"
+  ,"  #  o o #"
+  ,"### # ## #   ######"
+  ,"#   # ## #####  ..#"
+  ,"# o  o          ..#"
+  ,"##### ### #@##  ..#"
+  ,"    #     #########"
+  ,"    #######"
   ]
-
-prop_empty c1 = (c1::Int) == c1
-
-runTests = do
-  runTestTT testsSoko
-  quickCheck prop_empty
