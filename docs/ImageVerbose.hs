098d8285def74eb467004b45e5ea55674f3dc0c2
jonas <jonas@jonas-VirtualBox.(none)>
2012-06-20 21:58:47 +1030

    added images for GUI version added example file for GTK2HS/Cairo image handling

---
+++ docs/ImageVerbose.hs
@@ -0,0 +1,54 @@
+-- ImageVerbose from Haskell Wolfenstein
+-- source: http://code.google.com/p/haskellstolfenwein/source/browse/trunk/beispiele/09Images/ImageVerbose.hs?spec=svn8&r=8
+
+module Main (main) where
+
+{-
+import Graphics.Rendering.Cairo(Surface, setSourceSurface, paint, imageSurfaceCreateFromPNG)
+import Graphics.UI.Gtk(Window,AttrOp((:=)),Requisition(..)
+                      ,WindowPosition(WinPosCenter)
+                      ,initGUI,windowNew,onExpose,onDestroy,mainQuit,set
+                      ,windowWindowPosition,onSizeRequest,widgetShowAll
+                      ,mainGUI,widgetGetDrawWindow,renderWithDrawable)
+import Graphics.UI.Gtk.Gdk.Events(Event)
+-}
+import Graphics.Rendering.Cairo
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Gdk.Events
+
+onExposeEvent:: Window-> Surface-> Event-> IO Bool
+onExposeEvent window image _ = do
+  cr <-widgetGetDrawWindow window
+  renderWithDrawable cr $ do
+    save
+    translate 100 100
+    rectangle 0 0 100 100 -- left right width height
+    clip
+    newPath
+    setSourceSurface image 0 0
+    paint
+    restore
+
+    save
+    translate 200 200
+    rectangle 100 100 100 100 -- left right width height
+    clip
+    newPath
+    setSourceSurface image 0 0
+    paint
+    restore
+  return True
+
+
+main:: IO ()
+main = do
+  image <-imageSurfaceCreateFromPNG "test.png"
+  initGUI
+  window <-windowNew
+  onExpose window (onExposeEvent window image)
+  onDestroy window mainQuit
+  set window [ windowWindowPosition := WinPosCenter ]
+  window `onSizeRequest` return (Requisition 320 250)
+  widgetShowAll window
+  mainGUI
+
